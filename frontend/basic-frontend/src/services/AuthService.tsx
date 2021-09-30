9e5bf34f2b70a9526577774280036a0567e44be2
amqndalai <e0325976@u.nus.edu>
2021-09-30 11:22:55 +0800

    models for fe

--- frontend/basic-frontend/src/services/AuthService.tsx
+++ frontend/basic-frontend/src/services/AuthService.tsx
@@ -1,9 +1,9 @@
-import { LoginReq, LoginRes } from 'basic-api-models'
-import { apiService } from '.'
+import { LoginReq, LoginRes } from "../models";
+import { apiService } from ".";
 
 export class AuthService {
   async login(request: LoginReq) {
-    const { data } = await apiService.post<LoginRes>('/auth/login', request);
+    const { data } = await apiService.post<LoginRes>("/auth/login", request);
     return data;
   }
 }
