8e7567fc2a11b799b66c3cdf32232a3c7889ed92
amqndalai <e0325976@u.nus.edu>
2021-09-30 15:01:07 +0800

    removed test service

--- frontend/basic-frontend/src/pages/CheckoutPage.tsx
+++ frontend/basic-frontend/src/pages/CheckoutPage.tsx
@@ -13,8 +13,8 @@ export const CheckoutPage = () => {
 
   const onInit = async () => {
     try {
-      const res = await testService.test();
-      console.log(res);
+      // const res = await testService.test();
+      // console.log(res);
     } catch (err: any) {
       notificationService.error(err.error, err.message);
     }
@@ -26,44 +26,49 @@ export const CheckoutPage = () => {
         Checkout Form
         <p />
         <div className="checkout-form">
-        <Form initialValues={{ remember: true }}>
-          <Form.Item
-            name="name"
-            rules={[{ required: true, message: "Please input your Name!" }]}
-          >
-            <Input
-              placeholder="Name"
-            />
-          </Form.Item>
-          <Form.Item
-            name="address"
-            rules={[{ required: true, message: "Please input your Address!" }]}
-          >
-            <Input
-              placeholder="Address"
-            />
-          </Form.Item>
-          <Form.Item
-            name="email"
-            rules={[{ required: true, message: "Please enter a valid email address!" }]}
-          >
-            <Input
-              placeholder="Email"
-            />
-          </Form.Item>
-          <Form.Item>
-            <Form.Item name="t&c" valuePropName="checked" noStyle>
-              <Checkbox>I've read the terms and conditions</Checkbox>
+          <Form initialValues={{ remember: true }}>
+            <Form.Item
+              name="name"
+              rules={[{ required: true, message: "Please input your Name!" }]}
+            >
+              <Input placeholder="Name" />
             </Form.Item>
-          </Form.Item>
+            <Form.Item
+              name="address"
+              rules={[
+                { required: true, message: "Please input your Address!" },
+              ]}
+            >
+              <Input placeholder="Address" />
+            </Form.Item>
+            <Form.Item
+              name="email"
+              rules={[
+                {
+                  required: true,
+                  message: "Please enter a valid email address!",
+                },
+              ]}
+            >
+              <Input placeholder="Email" />
+            </Form.Item>
+            <Form.Item>
+              <Form.Item name="t&c" valuePropName="checked" noStyle>
+                <Checkbox>I've read the terms and conditions</Checkbox>
+              </Form.Item>
+            </Form.Item>
 
-          <Form.Item>
-            <Button type="primary" htmlType="submit" style={{ width: "100%" }}>
-              Checkout
-            </Button>
-          </Form.Item>
-        </Form>
-      </div>
+            <Form.Item>
+              <Button
+                type="primary"
+                htmlType="submit"
+                style={{ width: "100%" }}
+              >
+                Checkout
+              </Button>
+            </Form.Item>
+          </Form>
+        </div>
       </div>
     </BaseLayout>
   );
