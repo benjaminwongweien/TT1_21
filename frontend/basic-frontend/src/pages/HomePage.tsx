999a274faa41d56aaf2c6318c941936e95b15ee0
amqndalai <e0325976@u.nus.edu>
2021-09-30 16:32:36 +0800

    final commit

--- frontend/basic-frontend/src/pages/HomePage.tsx
+++ frontend/basic-frontend/src/pages/HomePage.tsx
@@ -13,10 +13,13 @@ export const HomePage = () => {
   }, []);
 
   const history = useHistory();
-  const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState<Array<Object>>([]);
   const onInit = async () => {
     try {
       const res = await productService.viewAllProducts();
+      var products = JSON.parse(JSON.stringify(res));
+      console.log(products);
+      setProducts(products);
     } catch (err: any) {
       notificationService.error(err.error, err.message);
     }
@@ -33,7 +36,7 @@ export const HomePage = () => {
                 cover={
                   <img
                     alt="example"
-                    src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+                    src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
                   />
                 }
                 actions={[
@@ -42,11 +45,9 @@ export const HomePage = () => {
                 ]}
               >
                 <Meta
-                  avatar={
-                    <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
-                  }
-                  title="Card title"
-                  description="This is the description"
+                  // title={product.title}
+                  title="Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops"
+                  description="Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday"
                 />
               </Card>
             </Col>
@@ -55,7 +56,7 @@ export const HomePage = () => {
                 cover={
                   <img
                     alt="example"
-                    src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+                    src="https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg"
                   />
                 }
                 actions={[
@@ -64,11 +65,8 @@ export const HomePage = () => {
                 ]}
               >
                 <Meta
-                  avatar={
-                    <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
-                  }
-                  title="Card title"
-                  description="This is the description"
+                  title="Mens Casual Premium Slim Fit T-Shirts "
+                  description="Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket."
                 />
               </Card>
             </Col>
@@ -77,7 +75,7 @@ export const HomePage = () => {
                 cover={
                   <img
                     alt="example"
-                    src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+                    src="https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg"
                   />
                 }
                 actions={[
@@ -86,11 +84,8 @@ export const HomePage = () => {
                 ]}
               >
                 <Meta
-                  avatar={
-                    <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
-                  }
-                  title="Card title"
-                  description="This is the description"
+                  title="Mens Cotton Jacket"
+                  description="great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions, such as working, hiking, camping, mountain/rock climbing, cycling, traveling or other outdoors. Good gift choice for you or your family member. A warm hearted love to Father, husband or son in this thanksgiving or Christmas Day."
                 />
               </Card>
             </Col>
@@ -99,7 +94,7 @@ export const HomePage = () => {
                 cover={
                   <img
                     alt="example"
-                    src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+                    src="https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg"
                   />
                 }
                 actions={[
@@ -108,11 +103,8 @@ export const HomePage = () => {
                 ]}
               >
                 <Meta
-                  avatar={
-                    <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
-                  }
-                  title="Card title"
-                  description="This is the description"
+                  title="Mens Casual Slim Fit"
+                  description="The color could be slightly different between on the screen and in practice. / Please note that body builds vary by person, therefore, detailed size information should be reviewed below on the product description."
                 />
               </Card>
             </Col>
