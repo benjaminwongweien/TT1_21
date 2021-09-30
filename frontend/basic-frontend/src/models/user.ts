035adf5361a3dff246aafa2beecf4abafa46bd17
amqndalai <e0325976@u.nus.edu>
2021-09-30 12:10:30 +0800

    models for fe

--- frontend/basic-frontend/src/models/user.ts
+++ frontend/basic-frontend/src/models/user.ts
@@ -1,9 +1,18 @@
 export interface LoginReq {
-    username: string;
-    password: string;
-  }
-  
-  export interface LoginRes {
-    token: string;
-  }
-  
+  username: string;
+  password: string;
+}
+
+export interface LoginRes {
+  token: string;
+}
+export interface Customer {
+  id: number;
+  username: string;
+  password: string;
+  first_name: string;
+  last_name: string;
+  postal_code: number;
+  gender: string;
+  created_at: string;
+}
