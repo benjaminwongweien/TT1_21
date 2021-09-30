d6f0e9896568d97100d453fc531bdb0b0b7c54b8
amqndalai <e0325976@u.nus.edu>
2021-09-30 16:02:44 +0800

    home page changes

--- frontend/basic-frontend/src/services/ProductService.tsx
+++ frontend/basic-frontend/src/services/ProductService.tsx
@@ -2,7 +2,7 @@ import { apiService } from ".";
 
 export class ProductService {
   async viewAllProducts() {
-    const { data } = await apiService.get<string>("/products/");
+    const data = await apiService.get<string>("/products/");
     return data;
   }
 }
