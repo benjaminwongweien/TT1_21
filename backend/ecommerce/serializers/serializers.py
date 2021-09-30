71dfba1df21ee4eb21c18cb8561fa0989ad237a2
Benjamin Wong Wei En <54574922+benjaminwongweien@users.noreply.github.com>
2021-09-30 15:22:58 +0800

    added endpoints

--- backend/ecommerce/serializers/serializers.py
+++ backend/ecommerce/serializers/serializers.py
@@ -2,39 +2,55 @@ from re import L
 from rest_framework import serializers
 from rest_framework.fields import SerializerMethodField
 from rest_framework.serializers import ModelSerializer
-from ..models import EcommerceProduct, EcommerceOrderItem
+from ..models import EcommerceOrder, EcommerceProduct, EcommerceOrderItem
+
 
 class ProductSerializer(ModelSerializer):
+
+    class Meta:
+
+        model = EcommerceProduct
+
+        fields = "__all__"
+
+class EcommerceOrderS(ModelSerializer):
     
     class Meta:
         
-        model = EcommerceProduct
+        model = EcommerceOrder
         
         fields = "__all__"
-        
-        
+
 class OrderCreateSerializer(ModelSerializer):
-    
+
     class Meta:
-        
+
         model = EcommerceOrderItem
-        
-        fields="__all__"
+
+        fields = "__all__"
 
 
 class OrderGetSerializer(ModelSerializer):
-    
+
     class Meta:
-        
+
         model = EcommerceOrderItem
-        
-        fields="__all__"
-        
-        
+
+        fields = "__all__"
+
+
 class OrderRemoveSerializer(ModelSerializer):
-    
+
     class Meta:
-        
+
+        model = EcommerceOrder
+
+        fields = "pk"
+
+class OrderItemRemoveSerializer(ModelSerializer):
+
+    class Meta:
+
         model = EcommerceOrderItem
-        
-        fields = []
+
+        fields = "pk"
