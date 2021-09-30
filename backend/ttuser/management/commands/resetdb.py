3c88f5eeecc5ea2c71306c759046187cac268368
Benjamin Wong Wei En <54574922+benjaminwongweien@users.noreply.github.com>
2021-09-30 13:02:31 +0800

    Added in the DB

--- backend/ttuser/management/commands/resetdb.py
+++ backend/ttuser/management/commands/resetdb.py
@@ -1,5 +1,7 @@
 # File Operations
-import os, glob, shutil
+import os
+import glob
+import shutil
 
 # Get the Platform
 from sys import platform
@@ -13,48 +15,55 @@ from django.contrib.sites.models import Site
 # Djnago Command
 from django.core.management.base import BaseCommand
 
+import json
+
+from ecommerce.models import EcommerceCategory, EcommerceOrder, EcommerceProduct, EcommerceOrderItem
+from allauth.account.models import EmailAddress
+
+
 class Command(BaseCommand):
     """
     Django Manage.py Command
-    
+
     Resets the Database By:
     (1) Deleting the Database File (If any)
     (2) Deleting All Migrations
     (3) Running Make Migrations
     (4) Running Migrate
     (5) Creating One Super User
-    
+
     """
+
     def add_arguments(self, parser):
-        
+
         parser.add_argument(
-            '-sdn', '--site_domain_name',  
-            dest  = "domain",
-            type  = str,
-            default = "",
-            help = "Domain Name of the Site"
+            '-sdn', '--site_domain_name',
+            dest="domain",
+            type=str,
+            default="",
+            help="Domain Name of the Site"
         ),
-        
+
         parser.add_argument(
-            '-sn', '--site_name',  
-            dest  = "name",
-            type  = str,
-            default = "",
-            help = "Name of the Site"
+            '-sn', '--site_name',
+            dest="name",
+            type=str,
+            default="",
+            help="Name of the Site"
         ),
-    
+
     help = 'Resets the database'
 
     def handle(self, *args, **options) -> None:
         """
         Handles the Resetting of Database
-        
+
         :param self: self
         """
-        
+
         print("[1] Getting the Name of the Database")
         dbname = settings.DATABASES["default"]["NAME"]
-        
+
         print("[2] Attempting to Delete the Database")
         try:
             os.remove(dbname)
@@ -67,7 +76,7 @@ class Command(BaseCommand):
 
         for migration in migrations:
             shutil.rmtree(migration)
-            
+
         if platform == "linux" or platform == "linux2":
             # Linux
             apps = [migration.split("/")[-2] for migration in migrations]
@@ -80,10 +89,10 @@ class Command(BaseCommand):
         print("[4] Making Migrations")
         for app in apps:
             os.system("python manage.py makemigrations %s" % app)
-            
+
         print("[5] Migrating")
         os.system("python manage.py migrate")
-        
+
         if options["name"] and options["domain"]:
             print("[6] Changing the Default Site Domain and Name")
             site = Site.objects.all().first()
@@ -91,21 +100,70 @@ class Command(BaseCommand):
             site.name = options["domain"]
             site.save()
         else:
-            print("[SKIP] Changing the Default Site Domain and Name (either field is blank)")
+            print(
+                "[SKIP] Changing the Default Site Domain and Name (either field is blank)")
             print("Site Name  :", options["name"])
             print("Domain Name:", options["domain"])
-        
+
         print("[7] Creating Super User")
         email, username, password = "test@test.test", "admin", "admin"
         print("E-mail  :", email)
         print("Username:", username)
         print("Password:", password)
 
+        print("[8] Creating Tables")
+
+        from django.contrib.auth import get_user_model
+
+        User = get_user_model()
+
+        with open("./data/customers.json", encoding="utf8") as customers:
+            data = json.loads(customers.read())
+            for x in data:
+                user = User.objects.create_user(
+                    username=x["username"],
+                    password=x["password"],
+                    email=x["username"] + "@gmail.com"
+                )
+                user.first_name = x["first_name"]
+                user.last_name = x["last_name"]
+                user.postal_code = x["postal_code"]
+                user.gender = x["gender"]
+                user.save()
+
+                EmailAddress.objects.create(
+                    user=user,
+                    email=x["username"] + "@gmail.com",
+                    primary=True,
+                    verified=True
+                ),
+
+        with open("./data/categories.json", encoding="utf8") as categories:
+            data = json.loads(categories.read())
+            for x in data:
+                EcommerceCategory.objects.create(
+                    **x
+                )
+
+        with open("./data/products.json", encoding="utf8") as products:
+            data = json.loads(products.read())
+            
+            for x in data:
+                category = EcommerceCategory.objects.filter(pk=x["category_id"]).first()
+                EcommerceProduct.objects.create(
+                    title = x["title"],
+                    price = x["price"],
+                    description=x["description"],
+                    category_id = category,
+                    image = x["image"],
+                    qty=x["qty"]
+                )
+
         try:
             # create environment variable for password
             os.environ['DJANGO_SUPERUSER_PASSWORD'] = password
-            os.system(f"python manage.py createsuperuser --username {username} --noinput --email {email} --skip-checks")
-        
+            os.system(
+                f"python manage.py createsuperuser --username {username} --noinput --email {email} --skip-checks")
+
         except Exception as e:
             print(e)
-        
